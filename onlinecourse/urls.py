ab8ff6364f87413662f99ff31676300f86c882ba
narmeen.imran <nayyab.imran57@gmail.com>
2026-08-31 10:20:40 +0100

    Update URL pattern for show_exam_result view

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -6,5 +6,5 @@ app_name = 'onlinecourse'
 urlpatterns = [
     path('<int:course_id>/', views.course_details, name='course_details'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/submission/<int:submission_id>/show_exam_result/', views.show_exam_result, name='show_exam_result'),
+    path('course/<int:course_id>/submission/<int:submission_id>/show_exam_result/', views.show_exam_result, name='show_exam_result'),
 ]
